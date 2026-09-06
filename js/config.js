62fd8c668567ba04e4ca9a1fc84f5193333bdbb6
Daniel16B <126487445+Daniel16B@users.noreply.github.com>
2026-09-06 14:58:20 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ const PARTY_CONFIG = {
 const GUESTS = {
   dima: {
     name: "Дима",
-    photo: "/birthday/images/maga.jpg",
+    photo: "images/dima.jpg",
     greeting: "Будем ждать тебя ❤️"
   },
   sasha: {
